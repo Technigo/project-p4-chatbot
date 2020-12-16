a02258617127c4aa12bda8374ada3612ba652715
Doberman <doberman@MacBook-Pro-som-tillhor-Doberman.local>
2020-12-16 16:09:29 +0100

    My very first commit

--- code/script.js
+++ code/script.js
@@ -1,5 +1,8 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const ageForm = document.getElementById('name-form')
+const ageInput = document.getElementById('name-input')
+const inputWrapper = document.getElementById('input-wrapper')
 
 // Global variables, if you need any, declared here
 
@@ -32,11 +35,40 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hi, I'm Glenn. I have understood that in Sweden all the pun masters are named Glenn. 
+  But since my employers were instructed to cheer people up in english – They hired me.`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
+  setTimeout(() => showMessage(`Anyway, I'm Glenn Close.`, 'bot'), 3000)
+  setTimeout(() => showMessage(`How old are you?`, 'bot'), 5000)
+}
+
+// Question 2
+const grapeQuestion = (event) => {
+  showMessage("Have you heard about the baby grape that didn't want to be made into wine?", 'bot')
+  inputWrapper.innerHTML = `
+  <button id="yesButton">YES</button>
+  <button id="noButton">NO</button>
+`
+  document.getElementById("yesButton").addEventListener('click', () => showMessage("Yes", 'user'))
+  document.getElementById("noButton").addEventListener('click', () => showMessage("No", 'user'))
+
+}
+
+// Question 1
+const handleAgeInput = (event) => {
+event.preventDefault()
+const userAge = ageInput.value
+showMessage(userAge, 'user')
+ageInput.value = ""
+setTimeout(() => showMessage(`Ah ok, ${userAge} and getting older huh. Well here's a wine joke that only gets better with age...`, 'bot'), 2000)
+setTimeout(() => grapeQuestion(event), 5000)
 }
 
 // Set up your eventlisteners here
+
+ageForm.addEventListener('submit', handleAgeInput)
+
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
