0e1f8b755b5d5c04b942490e80aa243fabf2d97d
Annaofdoom <32620147+Annaofdoom@users.noreply.github.com>
2020-12-20 22:23:00 +0100

    Added the answers but the links doesn't work yet

--- code/script.js
+++ code/script.js
@@ -1,20 +1,20 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
-
+const chat = document.getElementById("chat")
+const nameForm = document.getElementById("name-form")
 const inputwrapper = document.getElementById("input-wrapper")
 
 // Global variables, if you need any, declared here
 
 // Functions declared here
 
-const nameInput = document.getElementById('name-input')
+const nameInput = document.getElementById("name-input")
 
 const handleNameInput = (event) => {
   event.preventDefault()
 
   showMessage(nameInput.value, "user")
 
-  nameInput.value = ''
+  nameInput.value = ""
 
   setTimeout(()=> question1(), 1000)
 
@@ -23,46 +23,63 @@ const handleNameInput = (event) => {
   }
 
   const question1 = () => {
-    showMessage('Hur ofta vill du spara?', 'bot')
+    showMessage("Hur ofta vill du spara?", "bot")
     inputwrapper.innerHTML = `
-    <button id='button1'>Lite varje månad</button>
-    <button id='button2'>Ett engångsbelopp</button>`
-    document.getElementById('button1')
-    .addEventListener('click', () => question2())
-    document.getElementById('button2')
-    .addEventListener('click', () => answer1())
+    <button id="button1">Lite varje månad</button>
+    <button id="button2">Ett engångsbelopp</button>`
+    document.getElementById("button1")
+    .addEventListener("click", () => question2())
+    document.getElementById("button2")
+    .addEventListener("click", () => answer1())
 
   }
 
   const question2 = () => {
-    showMessage('Strålande! Att spara lite varje månad gör att du sprider riskerna. Hur mycket vill du spara i månaden?', 'bot')
+    showMessage("Strålande! Att spara lite varje månad gör att du sprider riskerna. Hur mycket vill du spara i månaden?", "bot")
     inputwrapper.innerHTML = `
-    <button id='button3'>Över 500 kr</button>
-    <button id='button4'>Under 500 kr</button>`
-    document.getElementById('button3')
-    .addEventListener('click', () => question3())
-    document.getElementById('button4')
-    .addEventListener('click', () => answer1())
+    <button id="button3">Över 500 kr</button>
+    <button id="button4">Under 500 kr</button>`
+    document.getElementById("button3")
+    .addEventListener("click", () => question3())
+    document.getElementById("button4")
+    .addEventListener("click", () => answer1())
   }
 
   const question3 = () => {
-    showMessage('Så den sista och kanske viktigaste frågan! Vad brinner du mest för?', 'bot')
+    showMessage("Så den sista och kanske viktigaste frågan! Vad brinner du mest för?", "bot")
     inputwrapper.innerHTML = `
-    <button id='button5'>Miljö & klimat</button>
-    <button id='button6'>Sociala frågor</button>
-    <button id='button7'>Jämställdhet</button>`
-    document.getElementById('button5')
-    .addEventListener('click', () => answer2())
-    document.getElementById('button6')
-    .addEventListener('click', () => answer2())
-    document.getElementById('button7')
-    .addEventListener('click', () => answer3())
+    <button id="button5">Miljö & klimat</button>
+    <button id="button6">Sociala frågor</button>
+    <button id="button7">Jämställdhet</button>`
+    document.getElementById("button5")
+    .addEventListener("click", () => answer2())
+    document.getElementById("button6")
+    .addEventListener("click", () => answer2())
+    document.getElementById("button7")
+    .addEventListener("click", () => answer3())
   }
 
+  const answer1 = () => {
+    showMessage("Hållbara aktier kanske passar dig!", "bot")
+    inputwrapper.innerHTML = `
+    <button oncklick=("window.location.href='https://www.aktieinvest.se/hallbart-sparande/hallbara-aktier'")>Läs mer om hållbara aktier!</button>`
+  }
+
+  const answer2 = () => {
+    showMessage("Hållbarhetsboxen kanske passar dig!", "bot")
+    inputwrapper.innerHTML = `
+    <button oncklick=("window.location.href='https://www.aktieinvest.se/hallbarhetsboxen'")>Läs mer om hållbarhetsboxen!</button>`
+  }
+
+  const answer3 = () => {
+    showMessage("Framtidsfeministens sparbox kanske passar dig!", "bot")
+    inputwrapper.innerHTML = `
+    <button oncklick=("window.location.href='https://www.aktieinvest.se/framtidsfeministen'")>Läs mer om Framtidsfeministens sparbox!</button>`
+  }
 
 // This function will add a chat bubble in the correct place based on who the sender is
 function showMessage(message, sender) {
-  if (sender === 'user') {
+  if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -71,7 +88,7 @@ function showMessage(message, sender) {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-  } else if (sender === 'bot') {
+  } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/aiv-bot_400x400.jpg" alt="Bot" />
@@ -87,8 +104,8 @@ function showMessage(message, sender) {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hej på dig! Vad heter du?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage(`Hej på dig! Vad heter du?`, "bot")
+  // Just to check it out, change "bot" to "user" here 👆
 }
 
 // Set up your eventlisteners here
